e0b6e95c24c20d37df507c1b55c2881dea48bc1a
Matheus Bozzi <matheusabreubozzi@gmail.com>
2021-03-30 15:44:49 -0300

    saida formatada

--- UDPPingClient.py
+++ UDPPingClient.py
@@ -4,57 +4,58 @@ from time import time, ctime
 from array import *
 from statistics import *
 
-N = 10
-dataRtt = []
-lostRtt = []
+N = 10   # numero de mensagens a serem enviadas
+dataRtt = [] # vetor com os dados de rtt que chegaram
+lostRtt = [] # vetor com os dados de rtt que foram perdidos
 
 if (len(sys.argv) != 3):
     print("Digite: UDPPingClient.py <server_host> <server_port>")
     sys.exit()
 
-name = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+# usuario digita o nome ate 30 caracteres
+name = ''.rjust(31, '#')
 while len(name) >= 30:
     name = input('Digite seu nome(ate 30 caracteres): \n')
 
-# Preparing the socket
+# pegando os dados do argv com o IP e porta do servidor
 serverHost, serverPort = sys.argv[1:]
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-clientSocket.settimeout(1)
+clientSocket = socket(AF_INET, SOCK_DGRAM)  # Inicialização do socket
+clientSocket.settimeout(1)  # definindo tempo de excessao
 
+# definindo o laço de quantas mensagens serão enviadas pelo cliente
 for i in range(N):
-    startTime = time()
-    message = str(i+1) + " 0" + " tmtm " + name
+    initTime = time()  # define tempo de inicio
+    message = str(i+1).zfill(5) + "0" + \
+        str(initTime)[11:15] + name.rjust(30, '#') # mensagem segundo o protocolo especificado
 
     try:
-        # Sending the message and waiting for the answer
         clientSocket.sendto(message.encode('utf-8'),
-                            (serverHost, int(serverPort)))
-        msgRetornada, serverAddress = clientSocket.recvfrom(1024)
+                            (serverHost, int(serverPort)))  # enviando a msg para o servidor
+        msgRetornada, serverAddress = clientSocket.recvfrom(1024) # esperando a resposta
 
-        # Checking the current time and if the server answered
-        endTime = time()
-        rtt = ((endTime - startTime)*1000)
+        endTime = time() # tempo da mensagem retornada (se o servidor respondeu)
+        rtt = ((endTime - initTime)*1000) # rtt
         if rtt > 1:
-            print("Pacote perdido\n")
-            lostRtt.append(rtt)
+            print("Pacote perdido\n")   # pacote perdido
+            lostRtt.append(rtt)        # vetor do tempo das mensagens atrasadas
         else:
-            msgretornada = msgRetornada.decode()
-            print(msgretornada)
-            print("RTT: %.3f ms\n" % rtt)
-            dataRtt.append(rtt)
+            msgretornada = msgRetornada.decode()  # mensagem retornada decodificada
+            print(msgretornada) 
+            print("RTT: %.3f ms\n" % rtt) 
+            dataRtt.append(rtt)            # vetor com tempo das mensagens que chegaram a tempo
 
     except timeout:
-        print("Pacote perdido\n")
+        print("%i Pacote perdido\n" % (i+1))   # pacote perdido 
 
-clientSocket.close()
+clientSocket.close() 
 
+# abaixo é feito a formatação da saída, com os calculos adequados
 if(len(dataRtt) > 0):
     print(N, ' packets transmitted,', len(dataRtt), 'received, ',
           (100 - ((len(dataRtt)/N)*100)), '% packet loss, time ', sum(dataRtt)+sum(lostRtt))
-    print('rtt min ', min(dataRtt))  # minimo rtt
-    print('avg ', sum(dataRtt)/len(dataRtt))  # media
-    print('rtt max ', max(dataRtt))  # maximo rtt
-    print('mdev ', pstdev(dataRtt))  # desvio populacional
-    # print('mdev ',stdev(dataRtt))
+    print('Rtt min ', min(dataRtt))  # minimo rtt
+    print('Avg ', sum(dataRtt)/len(dataRtt))  # media
+    print('Rtt max ', max(dataRtt))  # maximo rtt
+    print('Mdev ', stdev(dataRtt))  # desvio padrao
 else:
     print('100% packet loss')
---
+++ t.py
@@ -0,0 +1,5 @@
+from time import time, ctime
+
+for i in range(10):
+    start = time()
+    print(str(start))
